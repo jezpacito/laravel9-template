68ed2b30bc64342a5ac9a989f3de53ba1e281aa6
jezreelgatmaitan-cp <jezreel.gatmaitan@coreproc.com>
2023-01-28 18:43:58 +0800

    initial migration

---
+++ database/migrations/2023_01_28_103430_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('product_number');
+            $table->string('product_name');
+            $table->string('description')->nullable();
+            $table->integer('stocks_available')->default(0);
+            $table->integer('remaining_stocks')->default(0);
+            $table->boolean('status') ->default(0);
+            $table->integer('wholesale_price')->default(0);
+            $table->integer('regular_price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
